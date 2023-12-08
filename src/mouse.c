38a8ea162c58303fd9e636c52aa2a56453f2f115
semickolon <a9iqln3u@anonaddy.me>
2023-12-09 00:56:31 +0800

    Fix mouse scrolling speed

--- src/mouse.c
+++ src/mouse.c
@@ -1,5 +1,14 @@
 #include "mouse.h"
 #include "usb.h"
+#include "time.h"
+
+// TODO: Expose these to Nickel
+#define MOUSE_MOVE_SPEED 4
+#define MOUSE_SCROLL_INTERVAL_MS 20
+
+// TODO: Convert to __xdata
+int8_t scroll_direction = 0;
+uint16_t scroll_at_time = 0;
 
 void mouse_handle_key(uint16_t custom_code, uint8_t down) {
     if (custom_code < 8) {
@@ -9,16 +18,29 @@ void mouse_handle_key(uint16_t custom_code, uint8_t down) {
     }
 
     switch (custom_code) {
-    case 8:  USB_EP3I_write(1, down * 4);  break; // Right
-    case 9:  USB_EP3I_write(1, -down * 4); break; // Left
-    case 10: USB_EP3I_write(2, down * 4);  break; // Down
-    case 11: USB_EP3I_write(2, -down * 4); break; // Up
-
-    case 12: USB_EP3I_write(3, down);      break; // Wheel up
-    case 13: USB_EP3I_write(3, -down);     break; // Wheel down
+    case 8:  USB_EP3I_write(1, down * MOUSE_MOVE_SPEED);  break; // Right
+    case 9:  USB_EP3I_write(1, -down * MOUSE_MOVE_SPEED); break; // Left
+    case 10: USB_EP3I_write(2, down * MOUSE_MOVE_SPEED);  break; // Down
+    case 11: USB_EP3I_write(2, -down * MOUSE_MOVE_SPEED); break; // Up
+    case 12: // Wheel up
+    case 13: // Wheel down
+        scroll_direction = custom_code == 12 ? down : -down;
+        scroll_at_time = get_timer();
+        break;
     }
 }
 
 void mouse_process() {
+    if (scroll_direction != 0) {
+        if (get_timer() >= scroll_at_time) {
+            USB_EP3I_write(3, scroll_direction);
+            USB_EP3I_send_now();
+            USB_EP3I_write(3, 0);
+            scroll_at_time = get_timer() + MOUSE_SCROLL_INTERVAL_MS;
+        }
+    } else {
+        USB_EP3I_write(3, 0);
+    }
+
     USB_EP3I_send_now();
 }
